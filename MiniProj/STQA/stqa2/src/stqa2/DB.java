8e305ff6848760499313c706d568c807eaa81215
Omkar2310 <mankar.omkar2310@gmail.com>
2020-12-21 20:39:45 +0530

    added miniprojects of DMW and STQA

---
+++ MiniProj/STQA/stqa2/src/stqa2/DB.java
@@ -0,0 +1,15 @@
+package stqa2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","rootpasswordgiven");
+		}catch(Exception e){System.out.println(e);}
+		return con;
+	}
+
+}
